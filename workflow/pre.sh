004ff083737c3c789fed9d48c2ba71c6edb38f82
macwinnie <dev@macwinnie.me>
2022-11-29 15:38:43 +0100

    updated GitHub Actions WF

--- workflow/pre.sh
+++ workflow/pre.sh
@@ -13,14 +13,14 @@ echo "Branches / Apps are the next ToDo ... not implemented yet ..."
 
 # get image name for current branch
 echo "First of all, we are building the image base image “${master_image_name}”"
-echo "::set-output name=base_image::${master_image_name}"
+echo "base_image=${master_image_name}" >> $GITHUB_OUTPUT
 
 # get build-args for current branch
 json=$( cat ./jdk_versions.json | jq -c )
 echo "This list of tags will be built with Eclipse Adoptium OpenJDK version installed:"
 echo "$json" | jq -r 'keys[] as $k | "   ◆ \($k):\t\(.[$k]) "'
-echo "::set-output name=tag_jdk::$( echo "$json" | jq 'keys[] as $k | { "\($k)": "JDK_NAME=\(.[$k])" }' | jq -sc add )"
-echo "::set-output name=tags::$( echo "$json" | jq -c '{ "tags": keys }' )"
+echo "tag_jdk=$( echo "$json" | jq 'keys[] as $k | { "\($k)": "JDK_NAME=\(.[$k])" }' | jq -sc add )" >> $GITHUB_OUTPUT
+echo "tags=$( echo "$json" | jq -c '{ "tags": keys }' )" >> $GITHUB_OUTPUT
 
 # change dir back to working directory
 cd "${workingDir}"
